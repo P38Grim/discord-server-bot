180a459fe430e318642c7b3415fd40eaaaade06c
ZayDocs <155326981+P38Grim@users.noreply.github.com>
2024-05-12 21:44:32 -0400

    Update messageCreate.js

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,25 +1,25 @@
 const client = require('../../index');
 const config = require('../../config/config.json');
-
+// made by zaydocs.dev for timmy
 module.exports = {
     name: "messageCreate"
 };
-
+// made by zaydocs.dev for timmy
 client.on('messageCreate', async (message) => {
     if (message.channel.type !== 0) return;
     if (message.author.bot) return;
     if (!message.content.startsWith(config.PREFIX)) return;
     if (!message.guild) return;
     if (!message.member) message.member = await message.guild.fetchMember(message);
-
+// made by zaydocs.dev for timmy
     const args = message.content.slice(config.PREFIX.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     if (cmd.length == 0) return;
-
+// made by zaydocs.dev for timmy
     let command = client.commands.get(cmd);
-
+// made by zaydocs.dev for timmy
     if (!command) command = client.commands.get(client.aliases.get(cmd))
-
+// made by zaydocs.dev for timmy
     if (command) {
         if (command.ownerOnly) {
             if (!config.OWNER.includes(message.member.id)) {
@@ -29,7 +29,7 @@ client.on('messageCreate', async (message) => {
                 return;
             }
         }
-
+// made by zaydocs.dev for timmy
         try {
             command.run(client, message, args);
         } catch (err) {
@@ -37,3 +37,4 @@ client.on('messageCreate', async (message) => {
         }
     }
 })
+// made by zaydocs.dev for timmy

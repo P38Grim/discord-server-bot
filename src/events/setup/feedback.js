1caf983a64b58ebd569ee6b37d3043f086814cfc
ZayDocs <155326981+P38Grim@users.noreply.github.com>
2024-05-12 21:45:37 -0400

    Update feedback.js

--- src/events/setup/feedback.js
+++ src/events/setup/feedback.js
@@ -1,18 +1,18 @@
 const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
 const client = require("../../index.js");
-
+// made by zaydocs.dev for timmy
 module.exports = {
     name: "feedbackLoaded"
 };
-
+// made by zaydocs.dev for timmy
 client.on('interactionCreate', async interaction => {
     if (!interaction.isButton()) return;
-
+// made by zaydocs.dev for timmy
     if (interaction.customId == 'feedback') {
         const modal = new ModalBuilder()
             .setCustomId('feedbackmodal')
             .setTitle('FeedBack Us 😀');
-
+// made by zaydocs.dev for timmy
         const feedback = new TextInputBuilder()
             .setCustomId('feedback')
             .setLabel("FeedBack")
@@ -20,11 +20,12 @@ client.on('interactionCreate', async interaction => {
             .setRequired(true)
             .setMinLength(5)
             .setMaxLength(1024);
-
+// made by zaydocs.dev for timmy
         const firstActionRow = new ActionRowBuilder().addComponents(feedback);
-
+// made by zaydocs.dev for timmy
         modal.addComponents(firstActionRow);
-
+// made by zaydocs.dev for timmy
         await interaction.showModal(modal);
     }
 })
+// made by zaydocs.dev for timmy

e8c14e50570546e8723f3303844845a898352e3e
ZayDocs <155326981+P38Grim@users.noreply.github.com>
2024-05-12 21:43:55 -0400

    Update watermark.js

--- src/console/watermark.js
+++ src/console/watermark.js
@@ -1,6 +1,6 @@
 const { stripIndent } = require("common-tags");
 const colors = require('colors')
-
+// made by zaydocs.dev for timmy
 console.log(stripIndent`
 --------------------------------------------------------------------------------------
 ░██████╗███████╗██████╗░██╗░░░██╗███████╗██████╗░  ██████╗░░█████╗░████████╗
